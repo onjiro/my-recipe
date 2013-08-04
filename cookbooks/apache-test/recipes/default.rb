48c444b0ccd43ec52f6cdce48e811ddc57f7ab88
onjiro <onjiro.mohyahya@gmail.com>
2013-08-05 07:09:28 +0900

    Update recipe apache-test

--- cookbooks/apache-test/recipes/default.rb
+++ cookbooks/apache-test/recipes/default.rb
@@ -6,22 +6,40 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+["php", "php-mysql", "php-mbstring", "php-pear", "mysql", "mysql-server", "httpd", "mod_ssl"].each do |dependency|
+  package dependency
+end
+
+# services
+["mysqld", "httpd"].each do |one|
+  service one do
+    action [:enable, :start]
+  end
+end
 service "iptables" do
-  action :disable
+  action [:disable, :stop]
 end
 
-directory "/var/www/html"
-
-file "/var/www/html/index.html" do
-  content <<-EOS
-    It works!!
-  EOS
+# psedo-checkout
+directory "var/www/apache-test"
+file "/var/www/apache-test/index.html" do
+  content "It works!!"
 end
 
-web_app "sample" do
-  server_name "192.168.56.10"
-  server_aliases []
-  docroot "/var/www/html"
-  allowoverwrite "All"
-  cookbook "apache2"
+# link
+unless File.symlink? "var/www/html"
+  directory "/var/www/html" do
+    action :delete
+    recursive true
+  end
+
+  link "/var/www/html" do
+    to "/var/www/apache-test"
+  end
+end
+
+# まじめにやるならopscode-cookbooks/databaseを使う
+unless system "mysql -D test_db -u root < /dev/null"
+  execute "echo 'create database test_db' | mysql -u root"
+  # todo migration
 end
